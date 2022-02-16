85ab4ba80ef0ab34e11eed81e9b69e5d5fa22ca9
minghuynh <krioton311@gmail.com>
2022-02-16 23:15:22 +0700

    new

--- main.cpp
+++ main.cpp
@@ -1,72 +1,68 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h>
-void Menu(int a[], int &n);
-void NhapMangRanDom(int a[], int &n)
-{
-	printf("Chao\n");
-	printf("Nhap so phan tu mang: ");
-	scanf("%d", &n);
-	srand(time(0));
-	for (int i=0; i<n; i++)
-	   a[i] = 1 + rand() % (100-1+1);
-}
-void XuatMang(int a[], int n)
-{
-	printf("Cac phan tu cua mang la:\n");
-	for (int i=0; i<n; i++)
-	   printf("%d\t", a[i]);
-	printf("\n");
-}
-void LuaChon2(int a[], int n)
-{
-	printf("Cac so chia het cho 4 ma khong chia het cho 5 la:\n");
-	for (int i=0; i<n; i++)
-		if (a[i]%4==0 && a[i]%5!=0) 
-		   printf("%d\t", a[i]);
-	printf("\n");
-}
-void Xoa(int a[], int &n, int i)
-{
-	for (int j=i; j<n-1; j++)
-	   a[j] = a[j+1];
-	n--; 
-}
-void LuaChon3(int a[], int &n)
-{
-	int x, nBandau=n;
-	printf("Nhap gia tri x can xoa: ");
-	scanf("%d", &x);
-	for (int i=0; i<n-1; i++)
-		if (a[i]==x) Xoa(a,n,i);
-	if (nBandau==n) printf("Khong tim thay so nguyen x\n");
-	else printf("Da xoa\n");
-}
+#include <math.h>
+void menu();
+void PTBac2();
+void LaiSuatNganHang();
 int main()
 {
-	int a[100], n;
-	NhapMangRanDom(a,n);
-	Menu(a,n);
+	menu();
 }
-void Menu(int a[], int &n)
+void menu()
 {
 	int x;
 	do
 	{
-		printf("----------------------------\n");
-		printf("1-XuatMang\n");
-	    printf("2-Nhung so chia het cho 4 ma khong chia het cho 5\n");
-    	printf("3-Xoa so nguyen\n");
-	    printf("Chon gia tri: ");
+		printf("-------------------------\n");
+		printf("Chon gia tri\n");
+		printf("1-Tinh phuong trinh bac 2\n2-Tinh lai suat ngan hang\n3-thoat\n");
+		printf("--------------------------\n");
+		printf("Nhap gia tri: ");
 	    scanf("%d", &x);
-	    switch (x)
+		switch(x)
 	    {
-		case 1: XuatMang(a,n); break;
-		case 2: LuaChon2(a,n); break;
-		case 3: LuaChon3(a,n); break;
-		default: printf("Gia tri khong hop le\n"); 
-    	}
+		   case 1: PTBac2(); break;
+		   case 2: LaiSuatNganHang(); break;
+		   default: break;
+	    }
 	}
-	while (x>=1 && x<=3);
+	while(x>=1 && x<=2);
 }
-
+void PTBac2()
+{
+	float a, b, c, denta;
+	printf("Nhap gia tri a, b, c: ");
+	scanf("%f%f%f", &a, &b, &c);
+	denta=pow(b,2) - 4*a*c;
+	if (denta>0)
+	{
+		float x1, x2;
+		x1=(-b+sqrt(denta))/(2*a);
+        x2=(-b-sqrt(denta))/(2*a);
+		printf("Phuong trinh co 2 nghiem la %g, %g\n", x1, x2);
+	}
+	else if(denta==0)
+	{
+		float x;
+		x=-b/(2*a);
+		printf("Phuong trinh co 1 nghiem la %g\n", x);
+	}
+	else if(denta<0)
+	   printf("Phuong trinh vo nghiem\n");
+}
+void LaiSuatNganHang()
+{
+	float v, l;
+	int n, y;
+	do
+	{
+		printf("Nhap so tien von, lai suat, so nam gui\n");
+	    scanf("%f%f%d", &v, &l, &n);
+	}
+	while (v<0 || n<0);
+	printf("Nam\tTong tien\n");
+	for (int i=1, y=2022; i<=n; i++)
+	{
+		y=y+1;
+		printf("%d\t %g\n", y, v*pow(1+l, i));
+	}
+}
